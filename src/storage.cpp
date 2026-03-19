8ffa99841819e9aff1c01e35490e06b1b2ce4168
topgrfss <topg.rfss@tutamail.com>
2026-03-19 11:41:04 +0300

    archbtw

--- src/storage.cpp
+++ src/storage.cpp
@@ -23,9 +23,11 @@ void Storage::remove(const uint32_t plu, uint32_t exp_date)
 
 void Storage::get_all() const
 {
-        for(unsigned int i = 0; i < count_; i++){
-                std::cout << i <<": name: " << items_[i].name << " plu: " << items_[i].plu << " exp date: " << items_[i].exp_date << "\n";
-        }
+	for (unsigned int i = 0; i < count_; i++) {
+		std::cout << i + 1 << ": name: " << items_[i].name
+			  << " plu: " << items_[i].plu
+			  << " exp date: " << items_[i].exp_date << "\n";
+	}
 }
 
 int Storage::get_id(const Item &item)
@@ -51,7 +53,7 @@ Item *Storage::find_item(const uint32_t plu, uint32_t exp_date)
 {
 	for (unsigned int i = 0; i < count_; i++) {
 		if (plu == items_[i].plu && exp_date == items_[i].exp_date) {
-                        return &items_[i];
+			return &items_[i];
 		}
 	}
 	return nullptr;
