f33e71c4abe998d1dc321a3b44b78e1d00107a2b
topgrfss <topg.rfss@tutamail.com>
2026-03-20 16:24:20 +0300

    no ui

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,5 @@
 #include "storage.hpp"
-#include <iostream>
 
-class UserInterface {
-    public:
-	UserInterface(Storage &db) : db_(&db)
-	{
-	}
-	void run()
-	{
-	}
-
-    private:
-	Storage *db_;
-};
 int main()
 {
-	Storage db;
-	UserInterface ui(db);
-	ui.run();
 }
